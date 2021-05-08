2ae901d301ba3340d2b9a85d48a79c60c7f97ee2
ChuncI-Iunmaru <pwalczak796@gmail.com>
2021-05-08 15:21:42 +0200

    Store log in component data; Save and retrieve log from local storage;

--- src/components/DebugLog.vue
+++ src/components/DebugLog.vue
@@ -12,19 +12,34 @@ export default {
   name: "DebugLog",
   data() {
     return {
-      separator: '; '
+      separator: '; ',
+      counter: 1,
+      logArchive: ""
     }
   },
   created() {
     this.$eventHub.$on('log-msg', this.addValueMsgToLog);
   },
-  beforeDestroy(){
+  mounted() {
+    let retrievedData = this.openStorage();
+    if (retrievedData) {
+      this.counter = retrievedData.counter;
+      this.logArchive = retrievedData.logArchive;
+    }
+    this.$refs.logbox.value = this.logArchive;
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy")
+    // Ten log w localstorage przetrwa odświeżanie strony, ale ponowne uruchomienie apki już nie
+    // (hot reload z ide też nie)
     this.$eventHub.$off('log-msg');
+    localStorage.removeItem('log_data');
   },
   methods: {
     addValueMsgToLog(msg) {
       console.log("addToLog uruchomione");
-      let logText = this.formatTimestamp(msg.timestamp) + this.separator
+      let logText = this.counter + this.separator
+        + this.formatTimestamp(msg.timestamp) + this.separator
         + msg.serverIndex + " - " + msg.serverName + this.separator;
       if (msg.type === "ADDED") {
         logText += "Dodano " + msg.changed + this.separator;
@@ -32,12 +47,27 @@ export default {
         logText += "Usunięto " + msg.changed + this.separator;
       }
       logText += ("[" + msg.current + "]");
-      this.$refs.logbox.value = this.$refs.logbox.value + logText + "\n";
-
+      this.counter++;
+      this.logArchive += logText + "\n";
+      this.$refs.logbox.value = this.logArchive;
+      console.log({
+        counter: this.counter,
+        logArchive: this.logArchive
+      });
+      this.saveStorage({
+        counter: this.counter,
+        logArchive: this.logArchive
+      });
     },
     formatTimestamp(stamp) {
       return stamp.getHours() + ":" + stamp.getMinutes() + ":" + stamp.getSeconds() + "." + stamp.getMilliseconds()
-    }
+    },
+    openStorage () {
+      return JSON.parse(localStorage.getItem('log_data'));
+    },
+    saveStorage (data) {
+      localStorage.setItem('log_data', JSON.stringify(data));
+    },
   }
 }
 </script>
